7714cbff5a908c3d9c0dac1350f45590932cea6c
tlgur <tlgur0206@naver.com>
2022-04-28 11:07:05 +0900

    읽기 기능 만들기

--- myDjango/helloDjango/polls/views.py
+++ myDjango/helloDjango/polls/views.py
@@ -1,10 +1,44 @@
+from pydoc_data.topics import topics
 from django.shortcuts import render, HttpResponse
 
+topics=[
+    {'id': 1, 'title' : 'routing', 'body':'Routing is ...'},
+    {'id': 2, 'title' : 'view', 'body':'View is ...'},
+    {'id': 3, 'title' : 'Model', 'body':'Model is ...'}
+]
+
+def HTMLTemplate(articleTag):
+    global topics
+    ol = ''
+    for topic in topics:
+        ol += f"<li><a href =\"/read/{topic['id']}\">{topic['title']}</a></li>"
+    return f"""
+    <html>
+    <body>
+        <h1><a href="/">Django</a></h1>
+        <ol>
+            {ol}
+        </ol>
+        {articleTag}
+    </body>
+    </html>
+    """
+
+
 def index(request):
-        return HttpResponse("Hello, Django!")
+    article = """
+    <h2>Welcome</h2>
+    Hello, Django
+    """
+    return HttpResponse(HTMLTemplate(article))
 
 def create(req):
     return HttpResponse("Create")
 
 def read(req, id):
-    return HttpResponse("Read" + id)
+    global topics
+    article = ''
+    for topic in topics:
+        if topic['id'] == int(id):
+            article = f"<h2>{topic['title']}</h2>{topic['body']}"
+    return HttpResponse(HTMLTemplate(article))
