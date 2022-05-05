550fea75af776be541767f44d2ffac3d0fb441d1
tlgur <tlgur0206@naver.com>
2022-05-05 11:44:58 +0900

    9~11강

--- myDjango/helloDjango/polls/views.py
+++ myDjango/helloDjango/polls/views.py
@@ -1,6 +1,9 @@
 from pydoc_data.topics import topics
-from django.shortcuts import render, HttpResponse
+from urllib import response
+from django.shortcuts import redirect, render, HttpResponse
+from django.views.decorators.csrf import csrf_exempt
 
+nextId = 4
 topics=[
     {'id': 1, 'title' : 'routing', 'body':'Routing is ...'},
     {'id': 2, 'title' : 'view', 'body':'View is ...'},
@@ -24,17 +27,39 @@ def HTMLTemplate(articleTag):
     </html>
     """
 
-
 def index(request):
     article = """
     <h2>Welcome</h2>
     Hello, Django
+    <ul>
+        <li><a href = "/create/">create</a></li>
     """
+    print("response : " + str(response))
     return HttpResponse(HTMLTemplate(article))
 
+@csrf_exempt
 def create(req):
-    return HttpResponse("Create")
+    if(req.method == "GET"):
+        article="""
+        <h2>Create</h2>
+        <form action = "/create/" method="post">
+            <p><input type="text" placehold="title" name="title"></p>
+            <p><input type="textarea" placehold="body" name="body"></p>
+            <button type="submit">제출</button>
+        </form>
+        """
+        return HttpResponse(HTMLTemplate(article))
+    elif(req.method == "POST"):
+        global nextId
+        newTopic = {"id":nextId, "title": req.POST['title'], "body" : req.POST['body']}
+        topics.append(newTopic)
+        url = "/read/" + str(nextId) +"/"
+        nextId+=1
+        response = HttpResponse(status=302)
+        response['Location'] = url
+        return response
 
+        
 def read(req, id):
     global topics
     article = ''
