5ab88364ee60b9479e79f485f9c7351e80648432
unknown <begin1009@naver.com>
2022-06-09 10:16:01 +0900

    Url Mapping Django

--- myDjango/myVenv/Scripts/activate.bat
+++ myDjango/myVenv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\tlgur\.vscode\src\likelion\MyDjango\myVenv
+set VIRTUAL_ENV=C:\Users\tlgur\.vscode\src\likelion\myDjango\myvenv
 
 if not defined PROMPT set PROMPT=$P$G
 
@@ -16,7 +16,7 @@ if defined _OLD_VIRTUAL_PROMPT set PROMPT=%_OLD_VIRTUAL_PROMPT%
 if defined _OLD_VIRTUAL_PYTHONHOME set PYTHONHOME=%_OLD_VIRTUAL_PYTHONHOME%
 
 set _OLD_VIRTUAL_PROMPT=%PROMPT%
-set PROMPT=(myVenv) %PROMPT%
+set PROMPT=(myvenv) %PROMPT%
 
 if defined PYTHONHOME set _OLD_VIRTUAL_PYTHONHOME=%PYTHONHOME%
 set PYTHONHOME=
