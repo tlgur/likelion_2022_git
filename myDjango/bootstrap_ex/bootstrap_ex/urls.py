6a58bdd1c499ff07fc6b56d8ce723e30ee0984be
unknown <begin1009@naver.com>
2022-06-09 11:56:28 +0900

    Template Lang Practice

--- myDjango/bootstrap_ex/bootstrap_ex/urls.py
+++ myDjango/bootstrap_ex/bootstrap_ex/urls.py
@@ -4,5 +4,6 @@ from bootapp import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home),
+    path('', views.home, name='home'),
+    path('about/', views.about, name='about')
 ]
